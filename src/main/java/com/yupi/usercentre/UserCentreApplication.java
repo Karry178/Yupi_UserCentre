40f7aea55ebfef1a9052153543e1d90c2f6e917e
Java_Project <Java@ymh.cn>
2025-09-22 14:37:58 +0800

    用户中心项目 前后端项目

---
+++ src/main/java/com/yupi/usercentre/UserCentreApplication.java
@@ -0,0 +1,13 @@
+package com.yupi.usercentre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserCentreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCentreApplication.class, args);
+	}
+
+}
